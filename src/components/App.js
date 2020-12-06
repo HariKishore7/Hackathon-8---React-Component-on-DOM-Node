2b18383f9c206a7150a1e83ff672d146f2722811
HariKishore7 <harikishoret99@gmail.com>
2020-12-06 09:21:58 +0530

    Changed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 class App extends React.Component{
     render(){
         return(
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            <p id="main">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         );
     }
 }
